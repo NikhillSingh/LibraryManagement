85efaa19cfc1ee46963d7d91c4c38c9ed94efed2
ac98926 <ac98926@8CG8163KZ4>
2021-11-03 10:14:39 +1300

    Add project files.

---
+++ LibraryManagement/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        [Key]
+        public string  ISBN { get; set; }
+
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public int YearPublished { get; set; }
+        public decimal Price { get; set; }
+        public string PublisherName { get; set; }
+
+        public Loan Loan { get; set; }
+
+    }
+}
---
+++ LibraryManagement/Models/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public class Loan
+    {
+        public int LoanID { get; set; }
+        public DateTime DateIssued { get; set; }
+        public DateTime? DateReturned { get; set; }
+        public string ISBN { get; set; }
+
+        public Book Book { get; set; }
+
+    }
+}
